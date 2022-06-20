2da6f391c9ae798119dcda1ba663adf154f97b9f
Brenda Campos <100169962+bbrendara@users.noreply.github.com>
2022-06-20 20:22:50 -0300

    Add files via upload
    QUESTÃO 1- Arquivo em C
    
    #include <stdio.h>
    #include <math.h>
    #include <stdlib.h>
    
    int main()
    {
    	int indice=13,soma=0,k=0;
    	
    	while(k<indice){
    		k = k + 1;
            soma = soma + k;
    	}
    	
    	printf("%d",soma);
    	
    	return(0);
    }

---
+++ Questao 1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int indice=13,soma=0,k=0;
+	
+	while(k<indice){
+		k = k + 1;
+        soma = soma + k;
+	}
+	
+	printf("%d",soma);
+	
+	return(0);
+}
